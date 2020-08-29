7fe3ebc20497151c31bdb37e9585d6a83c64e4e3
Munir Ahmed Elsangedy <elsangedy@gmail.com>
2020-08-29 19:03:47 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+type Left<L, R> = {
+  __type: 'Left'
+  // value: L
+  fold: <T = any>(isLeft: (left: L) => T, isRight: (right: R) => T) => T
+}
+
+type Right<L, R> = {
+  __type: 'Right'
+  // value: R
+  fold: <T = any>(isLeft: (left: L) => T, isRight: (right: R) => T) => T
+}
+
+export type Either<L, R> = Left<L, R> | Right<L, R>
+
+export const left = <L, R>(value: L): Left<L, R> => ({
+  __type: 'Left',
+  // value,
+  fold: (l, _) => l(value),
+})
+
+export const right = <L, R>(value: R): Right<L, R> => ({
+  __type: 'Right',
+  // value,
+  fold: (_, r) => r(value),
+})
+
+export const isLeft = <L, R>(either: Either<L, R>) => either.__type === 'Left'
+
+export const isRight = <L, R>(either: Either<L, R>) => either.__type === 'Right'
+
+export const getLeft = <L, R>(either: Either<L, R>): L => either.fold<L>(identity, noop)
+
+export const getRight = <L, R>(either: Either<L, R>): R => either.fold<R>(noop, identity)
+
+const noop = (..._: any[]): any => {}
+
+const identity = <T>(value: T): T => value
